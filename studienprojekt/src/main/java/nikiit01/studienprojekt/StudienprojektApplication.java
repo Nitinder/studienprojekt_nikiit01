0ec142e34c4b9273d6d1a88553acb2f98af751d5
Nitin Kishore <nitinkishore3951@gmail.com>
2018-12-26 11:41:25 +0100

    Replaced projekt

---
+++ studienprojekt/src/main/java/nikiit01/studienprojekt/StudienprojektApplication.java
@@ -0,0 +1,14 @@
+package nikiit01.studienprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudienprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudienprojektApplication.class, args);
+	}
+
+}
+
--- studienprojekt/src/test/java/nikiit01/studienprojekt/StudienprojektApplicationTests.java
+++ studienprojekt/src/test/java/nikiit01/studienprojekt/StudienprojektApplicationTests.java
@@ -1,4 +1,4 @@
-package hse.distsys.gettingstarted;
+package nikiit01.studienprojekt;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class GettingstartedApplicationTests {
+public class StudienprojektApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
 
 }
+
